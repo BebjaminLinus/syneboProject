b7a4a9477a91325c250cc7d52d7732a5844b9ac9
Benjamin Linus <timmnikulcha@gmail.com>
2020-02-04 04:58:53 +0200

    04.02.2020

--- Aura homework/Component1Controller.js
+++ Aura homework/Component1Controller.js
@@ -1,19 +1,5 @@
 ({
 	doInit: function(component, event, helper) {        
-        var name = component.get("v.clientname");
-        var action = component.get("c.getCLients");
-        action.setParams({
-            "Name":name
-        });        
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.clients", response.getReturnValue());
-            }
-            else {
-                console.log("Failed with state: " + state);
-            }
-        });       
-        $A.enqueueAction(action);		
-	}
+        helper.helperMethod(component, event, helper);   
+    }
 })
